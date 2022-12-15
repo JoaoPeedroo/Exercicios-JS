7acdd8ec073b69b5b4d9566363547fef70bcad06
Joao Pedro <joaodeelze@gmail.com>
2022-12-14 21:18:13 -0300

    modificacoes no ex 14

--- Exercicios-Udemy/EX014.js
+++ Exercicios-Udemy/EX014.js
@@ -7,19 +7,14 @@
 //Checar se o número é realmente um número = Retorna o próprio número
 //Use a função com números de 0 a 100
 function testadoraDeNumeros(num) {
-  if (Number.isInteger(num) == true) {
-    if (num % 3 === 0 && num % 5 === 0) {
-      return console.log("FizzBuzz");
-    } else if (num % 5 === 0) {
-      return console.log("Buzz");
-    } else if (num % 3 === 0) {
-      return console.log("Fizz");
-    } else {
-      return console.log(`${num} não é divisível por 3 ou 5`);
-    }
-  } else {
-      console.log('DIgite um número')
-  }
+    if (typeof num !== 'number') return num;
+    if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+    if (num % 3 === 0) return 'Fizz';
+    if (num % 5 === 0) return 'Buzz';
+    return num;
 }
 
-testadoraDeNumeros(e)
+console.log('a', testadoraDeNumeros('a'))
+for (let i = 0; i <= 100; i++) {
+  console.log(i, testadoraDeNumeros(i));
+}
